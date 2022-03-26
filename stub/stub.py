07195fe69709fccd8858158942a0d9111e402a79
Stephen Checkoway <s@pahtak.org>
2022-03-26 04:27:37 -0400

    Format code

--- stub/stub.py
+++ stub/stub.py
@@ -6,30 +6,34 @@ Extract the flasher stubs from esptool.py.
 Usage: IDF_PATH=/path/to/esp-idf ./stub.py
 """
 
-import base64
-import zlib
 import os
 import os.path
-from struct import pack
 import sys
+from struct import pack
 
-if 'IDF_PATH' not in os.environ:
-    print("Set the IDF_PATH environment variable (run the export.sh script in the esp-idf directory)",
-          file=sys.stderr)
+if "IDF_PATH" not in os.environ:
+    print(
+        "Set the IDF_PATH environment variable (run the export.sh script in the esp-idf directory)",
+        file=sys.stderr,
+    )
     sys.exit(1)
+
 
 class Module(object):
     """
     Satisfies all the requirements for the two serial modules.
     """
-    def __init__(self, defs = None):
+
+    def __init__(self, defs=None):
         self.__dict__ = defs or dict()
+
     def __getattr__(self, name):
         return self.__dict__[name]
 
-sys.modules['serial'] = Module()
-sys.modules['serial.tools'] = Module({'list_ports': lambda: None })
-sys.path.append(os.path.join(os.environ['IDF_PATH'], 'components/esptool_py/esptool'))
+
+sys.modules["serial"] = Module()
+sys.modules["serial.tools"] = Module({"list_ports": lambda: None})
+sys.path.append(os.path.join(os.environ["IDF_PATH"], "components/esptool_py/esptool"))
 try:
     import esptool
 except ImportError:
@@ -37,7 +41,8 @@ except ImportError:
     sys.exit(1)
 
 
-MAGIC = b'STUB'
+MAGIC = b"STUB"
+
 
 def image(stub, type):
     """
@@ -54,26 +59,26 @@ def image(stub, type):
     n+8: Data
     """
     # entry text_start text_len text data_start data_len data
-    entry = stub['entry']
-    text_start = stub['text_start']
-    text = stub['text']
-    data_start = stub['data_start']
-    data = stub['data']
+    entry = stub["entry"]
+    text_start = stub["text_start"]
+    text = stub["text"]
+    data_start = stub["data_start"]
+    data = stub["data"]
     im = bytearray(MAGIC)
-    im.extend(pack('<IIII', type, entry, text_start, len(text)))
+    im.extend(pack("<IIII", type, entry, text_start, len(text)))
     im.extend(text)
-    im.extend(pack('<II', data_start, len(data)))
+    im.extend(pack("<II", data_start, len(data)))
     im.extend(data)
     return im
 
-with open('esp8266_stub.bin', 'wb') as f:
-    f.write(image(esptool.ESP8266ROM.STUB_CODE, 0x10000))
-with open('esp32_stub.bin', 'wb') as f:
-    f.write(image(esptool.ESP32ROM.STUB_CODE, 0))
-with open('esp32s2_stub.bin', 'wb') as f:
-    f.write(image(esptool.ESP32S2ROM.STUB_CODE, 2))
-with open('esp32s3_stub.bin', 'wb') as f:
-    f.write(image(esptool.ESP32S3ROM.STUB_CODE, 9))
-with open('esp32c3_stub.bin', 'wb') as f:
-    f.write(image(esptool.ESP32C3ROM.STUB_CODE, 5))
 
+with open("esp8266_stub.bin", "wb") as f:
+    f.write(image(esptool.ESP8266ROM.STUB_CODE, 0x10000))
+with open("esp32_stub.bin", "wb") as f:
+    f.write(image(esptool.ESP32ROM.STUB_CODE, 0))
+with open("esp32s2_stub.bin", "wb") as f:
+    f.write(image(esptool.ESP32S2ROM.STUB_CODE, 2))
+with open("esp32s3_stub.bin", "wb") as f:
+    f.write(image(esptool.ESP32S3ROM.STUB_CODE, 9))
+with open("esp32c3_stub.bin", "wb") as f:
+    f.write(image(esptool.ESP32C3ROM.STUB_CODE, 5))
